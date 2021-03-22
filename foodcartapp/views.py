1b7bdbe46e8a16ac109a49ff76fc3f654e36ccba
Konstantin Zemlyakov <kostyamorales@gmail.com>
2021-03-22 19:34:16 +0300

    Rewrite data validation logic with DRF

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -3,10 +3,9 @@ from django.templatetags.static import static
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.serializers import ModelSerializer
 
-from .models import Order
-from .models import Product
+from .models import Order, Product, OrderItem
 
 
 def banners_list_api(request):
@@ -61,54 +60,31 @@ def product_list_api(request):
     })
 
 
+class OrderItemSerializer(ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['product', 'quantity']
+
+
+class OrderSerializer(ModelSerializer):
+    products = OrderItemSerializer(many=True, allow_empty=False)
+
+    class Meta:
+        model = Order
+        fields = ['firstname', 'lastname', 'address', 'phonenumber', 'products']
+
+
 @api_view(['POST'])
 def register_order(request):
-    order_details = request.data
-    products = Product.objects.all()
-    products_id = []
-    for product in products:
-        products_id.append(product.id)
-
-    if 'products' not in order_details.keys():
-        return Response(['products key not presented'], status=400)
-
-    if order_details['products'] is None:
-        return Response(['products key is None'], status=400)
-
-    if not isinstance(order_details['products'], list):
-        return Response(['products key not list'], status=400)
-
-    if not order_details['products']:
-        return Response(['products key does not matter'], status=400)
-
-    if 'firstname' and 'lastname' and 'phonenumber' and 'address' not in order_details.keys():
-        return Response(['no order keys'], status=400)
-
-    if order_details['firstname'] is None \
-        and order_details['lastname'] is None \
-        and order_details['phonenumber'] is None \
-        and order_details['address'] is None:
-        return Response(['all order keys are None'])
-
-    if order_details['firstname'] is None:
-        return Response(['firstname key is None'], status=400)
-
-    if not order_details['phonenumber']:
-        return Response(['phonenumber key does not matter'], status=400)
-
-    if order_details['products'][0]['product'] not in products_id:
-        return Response(['product number does not exist'], status=400)
-
-    if not isinstance(order_details['firstname'], str):
-        return Response(['products key not str(string)'], status=400)
-
+    serializer = OrderSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
     order = Order.objects.create(
-        firstname=order_details['firstname'],
-        lastname=order_details['lastname'],
-        address=order_details['address'],
-        phonenumber=order_details['phonenumber'],
+        firstname=serializer.validated_data['firstname'],
+        lastname=serializer.validated_data['lastname'],
+        address=serializer.validated_data['address'],
+        phonenumber=serializer.validated_data['phonenumber'],
     )
-    for commodity in order_details['products']:
-        product = get_object_or_404(Product, id=commodity['product'])
+    for commodity in serializer.validated_data['products']:
+        product = get_object_or_404(Product, name=commodity['product'])
         order.items.create(product=product, quantity=commodity['quantity'])
     return Response({})
