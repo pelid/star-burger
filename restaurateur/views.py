28fe613ee73cdcf0d8f7ca5ac53cedd3b9ac87b4
Konstantin Zemlyakov <kostyamorales@gmail.com>
2021-03-24 14:16:38 +0300

    Enable order editing for the manager

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -96,5 +96,6 @@ def view_restaurants(request):
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
     return render(request, template_name='order_items.html', context={
-        'order_items': Order.objects.annotate(full_price=Sum('items__price'))
+        'order_items': Order.objects.annotate(full_price=Sum('items__price')),
+        'opts': Order._meta
     })
