08c4a280bd539eed8f972bf9e8d20b4acccf330a
Konstantin Zemlyakov <kostyamorales@gmail.com>
2021-03-24 18:49:09 +0300

    Return to /manager/orders after editing the order in the Django Admin site

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.shortcuts import reverse
+from django.shortcuts import reverse, redirect
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import Product
 from .models import ProductCategory
@@ -125,3 +126,9 @@ class OrderItemAdmin(admin.ModelAdmin):
 class OrderAdmin(admin.ModelAdmin):
     inlines = [OrderItemInline]
     list_display = ('firstname', 'lastname', 'address', 'phonenumber',)
+
+    def response_change(self, request, obj):
+        res = super().response_change(request, obj)
+        if url_has_allowed_host_and_scheme(request.GET['next'], None):
+            return redirect(request.GET['next'])
+        return res
